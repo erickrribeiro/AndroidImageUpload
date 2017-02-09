d844084a6b3aefefbc5a9ad418c00398dc7b9d50
Erick Ribeiro <erick.ribeiro.16@gmail.com>
2017-02-09 14:41:41 -0400

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/androidimageupload/Constants.java
@@ -0,0 +1,11 @@
+package net.simplifiedcoding.androidimageupload;
+
+/**
+ * Created by Belal on 6/10/2016.
+ */
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.100.12/teewa/upload.php";
+    public static final String IMAGES_URL = "http://192.168.100.12/teewa/getImages.php";
+
+}
+//https://www.simplifiedcoding.net/android-upload-image-to-server/
